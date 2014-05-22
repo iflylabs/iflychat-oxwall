8b13fb48dd0beb3c941e758dd58705096d427fd4
Shashwat Srivastava <darklrd@gmail.com>
2014-05-22 15:50:08 +0530

    iFlyChat Oxwall - minor typo fix

--- controllers/iflychat.php
+++ controllers/iflychat.php
@@ -3,8 +3,8 @@
  * @package iFlyChat
  * @version 1.0.0
  * @copyright Copyright (C) 2014 iFlyChat. All rights reserved.
- * @license GNUiFlyChat/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
- * @author  Team
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ * @author iFlyChat Team
  * @link https://iflychat.com
  */
 
